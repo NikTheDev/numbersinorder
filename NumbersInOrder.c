80230fe7ea318c5783c163cd541a80e6f4590629
NikTheDev <75281500+NikTheDev@users.noreply.github.com>
2021-01-12 19:05:36 +0200

    Add files via upload

---
+++ NumbersInOrder.c
@@ -0,0 +1,65 @@
+/* This is a program made by NickTheDev for putting numbers in an order!
+  Enjoy the program!!
+
+   WARNING!!
+   This is an open-source program for GitHub Community only!!
+
+*/
+
+
+#include <stdio.h>
+
+main()
+{
+    //integers
+
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+    int f;
+    int g;
+    int h;
+
+    //Let's start the program
+    printf("Let's Put some numbers in our Code.Type the first number :");
+    scanf("%d" , &a);
+
+    printf("Give me the second number : ");
+    scanf("%d" , &b);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!! Give me another one : :");
+    scanf("%d" , &c);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!! Give me another one :");
+    scanf("%d" , &d);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!! Give me another one  :");
+    scanf("%d" , &e);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!! Give me another one  :");
+    scanf("%d" , &f);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!! Give me another one  :");
+    scanf("%d" , &g);
+
+    printf("At least 8 numbers you can give me.And you will give all of them!!!  Give me another one :");
+    scanf("%d" , &h);
+
+    //end of scanning
+
+    printf("Nice now i will add it in a sequel for you!!");
+
+    //the sequel organizing
+    //manual from the dev
+    printf(" \n Number1 : %d \n Number2 : %d \n Number3 : %d \n Number4 : %d \n Number5 : %d \n Number6 : %d \n Number7 : %d \n Number8 : %d \n " , a, b, c, d, e, f, g, h);
+
+    //end of organization
+
+    printf("Here you are...\nHope you like it!! \n");
+
+    return 0;
+
+    //end of program
+}
